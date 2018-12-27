d696bd3b3dc5fac25332c451e9c521157fc1ba30
jiangsir404 <2461805286@qq.com>
2018-12-27 14:59:26 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Colorprint
 用Python装饰器封装的一个实现logger+colorlog的颜色输出功能，不需要依赖第三方库
+
+## usage
+
+一共有两个类
+
+Logger类:
+
+	from colorprint import Logger
+    logger = Logger()
+    logger.info('test1')
+    logger.error('test2')
+    logger.debug('test3')
+    logger.warning('test4')
+    logger.critical('test5')
+    logger.success('test6')
+
+
+ColorPrint类
+
+    colorprint = ColorPrint()
+    colorprint.print_red_text('i am red')
+    colorprint.print_random_text('i am change color')
+    colorprint.print_random_text('i am change color')
+
+
+![1.jpg](1.jpg)
--- colorprint.py
+++ colorprint.py
@@ -9,52 +9,52 @@ class ColorPrint:
 
     def print_black_text(self, content):  # 200
         message = "\033[0;30m%s\033[0m" % (content)
-        return message
+        print message
 
     def print_red_text(self, content):  # 200
         message = "\033[1;31m%s\033[0m" % (content)
-        return message
+        print message
 
     def print_green_text(self, content):  # 200
         message = "\033[1;32m%s\033[0m" % (content)
-        return message
+        print message
 
     def print_yellow_text(self, content):  # 40x
         message = "\033[1;33m%s\033[0;m" % (content)
-        return message
+        print message
 
     def print_blue_text(self, content):  # 40x
         message = "\033[1;34m%s\033[0;m" % (content)
-        return message
+        print message
 
     def print_magenta_text(self, content):  # 30x
         message = "\033[1;35m%s\033[0;m" % (content)
-        return message
+        print message
 
     def print_cyan_text(self, content):  # 30x
         message = "\033[1;36m%s\033[0;m" % (content)
-        return message
+        print message
 
     def print_white_text(self, content):  # 30x
         message = "\033[1;37m%s\033[0;m" % (content)
-        return message
+        print message
 
     def print_reset_text(self, content):  # 30x
         message = "\033[1;38m%s\033[0;m" % (content)
-        return message
+        print message
 
 
-def print_random_text(content):
-    output = ColorPrint()
-    colors = {31: 'red', 32: 'green', 33: 'yellow', 34: 'blue', 35: 'magenta', 36: 'cyan', 37: 'white'}  # 抛弃了黑色
-    color = colors[random.randint(31, 37)]
-    # print color
-    getattr(output, 'print_%s_text' % color)(content)
+    def print_random_text(self,content):
+        colors = {31: 'red', 32: 'green', 33: 'yellow', 34: 'blue', 35: 'magenta', 36: 'cyan', 37: 'white'}  # 抛弃了黑色
+        color = colors[random.randint(31, 37)]
+        # print color
+        getattr(self, 'print_%s_text' % color)(content)
 
 
 log_colors = {
     'DEBUG': 'cyan',
-    'INFO': 'green',
+    'SUCCESS': 'green',
+    'INFO': 'blue',
     'WARNING': 'yellow',
     'ERROR': 'red',
     'CRITICAL': 'white'
@@ -78,6 +78,8 @@ def log(F):
             return getattr(output, 'print_%s_text' % log_colors['ERROR'])(content)
         elif F.__name__ == 'critical':
             return getattr(output, 'print_%s_text' % log_colors['CRITICAL'])(content)
+        elif F.__name__ == 'success':
+            return getattr(output, 'print_%s_text' % log_colors['SUCCESS'])(content)
 
     return wrapper
 
@@ -103,9 +105,23 @@ class Logger:
     def critical(self, content):
         return content
 
+    @log 
+    def success(self,content):
+        return content
+
 
 if __name__ == '__main__':
     logger = Logger()
-    print logger.info('test1')
-    print logger.error('test2')
-    print logger.debug('test3')
+    logger.info('test1')
+    logger.error('test2')
+    logger.debug('test3')
+    logger.warning('test4')
+    logger.critical('test5')
+    logger.success('test6')
+
+
+
+    colorprint = ColorPrint()
+    colorprint.print_red_text('i am red')
+    colorprint.print_random_text('i am change color')
+    colorprint.print_random_text('i am change color')
